8df2ec80300baaec4bcac7bda29d6b81f05a8ca1
satani99 <42287151+satani99@users.noreply.github.com>
2021-02-01 09:54:57 +0530

    Add files via upload

---
+++ jpg_to_png.py
@@ -0,0 +1,14 @@
+from PIL import Image, ImageFilter
+import sys
+import os
+
+img_path = sys.argv[1]
+new_path = sys.argv[2]
+
+if not os.path.exists(new_path):
+    os.makedirs(new_path)
+
+for file in os.listdir(img_path):
+    img = Image.open(f'{img_path}{file}')
+    new_file = os.path.splitext(file)[0]
+    img.save(f"{new_path}/{new_file}.png", "png")
